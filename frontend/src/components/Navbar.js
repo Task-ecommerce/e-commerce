3e9faa542f2c1c9b128e309d3b3b9844a7a25339
mehmet <sayinmehmet47@gmail.com>
2022-08-02 18:15:25 +0200

    navbar add

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import { MdOutlineLocalGroceryStore } from 'react-icons/md';
+
+const Wrapper = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  height: 60px;
+  background-color: #fff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 10;
+  @media (max-width: 768px) {
+    justify-content: center;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #333;
+  margin: 0;
+  @media (max-width: 768px) {
+    font-size: 1.2rem;
+  }
+`;
+
+const Store = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default function Navbar() {
+  return (
+    <Wrapper>
+      <Title>Bejamas</Title>
+      <Store>
+        <MdOutlineLocalGroceryStore size={30} />
+      </Store>
+    </Wrapper>
+  );
+}
