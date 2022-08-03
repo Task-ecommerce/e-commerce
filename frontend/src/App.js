ee2132e22954e8a3fcfcaf90197133986f18b173
MehmetCakir1 <mehmetcakir4227@gmail.com>
2022-08-03 15:16:39 +0300

    featured

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import FeaturedProduct from './components/FeaturedProduct';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <FeaturedProduct/>
     </div>
   );
 }
